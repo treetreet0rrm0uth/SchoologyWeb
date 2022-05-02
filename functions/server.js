6262fc4bbe7c0fb2fd4013d6c6c2f4849eff5f2d
unknown <heehyeonljeong@gmail.com>
2022-05-01 18:07:27 -0700

    commit

--- functions/server.js
+++ functions/server.js
@@ -5,8 +5,10 @@ const serverless = require("serverless-http")
 const ejs = require("ejs")
 const app = express()
 const router = express.Router()
+const path = require("path")
 const SchoologyWeb = new SchoologyAPI(process.env.key, process.env.secret)
 
+app.set('views', path.join(__dirname, 'views'))
 app.set("view engine", "ejs")
 
 app.engine('ejs', require('ejs').__express)
