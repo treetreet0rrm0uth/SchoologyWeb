887cd4e2fcadbd105d84e6df4d905ce1283d1ec4
unknown <heehyeonljeong@gmail.com>
2022-05-01 17:26:46 -0700

    commit

---
+++ functions/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const SchoologyAPI = require("schoologyapi")
+const express = require("express")
+const serverless = require("serverless-http")
+const app = express()
+const router = express.Router()
+const SchoologyWeb = new SchoologyAPI(process.env.key, process.env.secret)
+
+app.set("view engine", "ejs")
+
+router.get("/", (req, res) => {
+    res.render("index")
+})
+
+router.get("/execute/:district/:request", (req, res) => {
+    async function main() {
+        const token = await SchoologyWeb.createRequestToken()
+        res.redirect(`https://${req.params.district}.schoology.com/oauth/authorize?${token}?oauth_callback=https://schoologyweb.netlify.app/view`)
+    }
+    console.log(req.ip)
+    main()
+})
+
+router.get("/view/:request", (req, res) => {
+    console.log(req)
+})
+
+app.use("/", router)
+
+module.exports.handler = serverless(app)
