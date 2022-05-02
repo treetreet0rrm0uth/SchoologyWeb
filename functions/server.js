b6e957e1f902fe4a7d0a0df6d4e4b2ece99ca750
unknown <heehyeonljeong@gmail.com>
2022-05-01 17:46:12 -0700

    commit

--- functions/server.js
+++ functions/server.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs")
 app.engine('ejs', require('ejs').__express);
 
 router.get("/", (req, res) => {
-    res.render("index")
+    res.render("index.ejs")
 })
 
 router.get("/execute/:district/:request", (req, res) => {
